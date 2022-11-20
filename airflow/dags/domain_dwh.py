09ab01e13ad1f04636c89180773c83f29bd0c648
ajpotts01 <pottseh@gmail.com>
2022-11-20 22:35:58 +1100

    Latest Airflow

--- airflow/dags/domain_dwh.py
+++ airflow/dags/domain_dwh.py
@@ -12,27 +12,21 @@ with DAG(
     schedule_interval='0 6 * * *',
     start_date=pendulum.datetime(2022, 11, 11, tz="UTC"),
     catchup=False,
-    dagrun_timeout=datetime.timedelta(minutes=15),
+    dagrun_timeout=datetime.timedelta(minutes=60),
     tags=['domain']
 ) as workflow:
 
+    # region Connections/variables
     # Init Airbyte/Databricks connections
     # For some reason, using the Jinja templated way of grabbing variables can't find these.
     # Use Variable.get instead
+    
+    # Airbyte
     airbyte_airflow_conn_id = Variable.get("ajp_airbyte_connection")
     airbyte_domain_sync_id = Variable.get("airbyte_ajp_01_domain_api_sync")
 
-    databricks_abs_job_id = Variable.get("databricks_abs_job_id")
-    databricks_abs_job_name = Variable.get("databricks_abs_job_name")
-    databricks_domain_bronze_job_name = Variable.get("databricks_domain_bronze_job_name")
-    databricks_domain_bronze_job_id = Variable.get("databricks_domain_bronze_job_id")
-    databricks_domain_silver_job_name = Variable.get("databricks_domain_silver_job_name")
-    databricks_domain_silver_job_id = Variable.get("databricks_domain_silver_job_id")
-    databricks_airflow_conn = Variable.get("ajp_databricks_connection")
-
-    # The ABS notebook has a few datasets of interest that are set up as inputs
-    # to a widget in a common notebook.
-    # Set up tasks to call these once each, plus REGION and STATE dimensions.
+    # The ABS notebook has a few datasets of interest that are on different endpoints.
+    # This dictionary is used to dynamically generate the tasks required.
     airbyte_abs_endpoints = dict(
             med_age_persons=Variable.get("airbyte_ajp_01_abs_med_age_persons"),
             #count_households=Variable.get("airbyte_ajp_01_abs_count_households"),
@@ -41,13 +35,22 @@ with DAG(
             med_ttl_fam_income_weekly=Variable.get("airbyte_ajp_01_abs_med_ttl_fam_income_weekly")
         )
 
-    # databricks_abs_datasets = ['hhld_size', 'med_age', 'med_income', 'mortgage_pymt', 'rent_pymt']
 
-    # databricks_abs_params = [dict(
-    #     dataset = x,
-    #     dimension_1 = 'REGION',
-    #     dimension_2 = 'STATE'
-    # ) for x in databricks_abs_datasets]
+    databricks_airflow_conn = Variable.get("ajp_databricks_connection")
+    databricks_domain_bronze_job_name = Variable.get("databricks_domain_bronze_job_name")
+    databricks_domain_bronze_job_id = Variable.get("databricks_domain_bronze_job_id")
+    databricks_abs_bronze_job_name = Variable.get("databricks_abs_bronze_job_name")
+    databricks_abs_bronze_job_id = Variable.get("databricks_abs_bronze_job_id")    
+    databricks_domain_silver_job_name = Variable.get("databricks_domain_silver_job_name")
+    databricks_domain_silver_job_id = Variable.get("databricks_domain_silver_job_id")
+    databricks_abs_silver_job_name = Variable.get("databricks_abs_silver_job_name")
+    databricks_abs_silver_job_id = Variable.get("databricks_abs_silver_job_id")    
+    databricks_combined_gold_job_id = Variable.get("databricks_abs_domain_gold_job_id")
+    databricks_combined_gold_job_name = Variable.get("databricks_abs_domain_gold_job_name")
+
+    # endregion Connections/variables
+
+    # region Tasks
 
     # Task setup for Airbyte - Domain. Just one operator needed.
     airbyte_task_domain = AirbyteTriggerSyncOperator(
@@ -59,6 +62,7 @@ with DAG(
         wait_seconds=3
     )
 
+    # ABS tasks - dynamically created from airbyte_abs_endpoints dictionary
     airbyte_abs_tasks = [
         AirbyteTriggerSyncOperator(
             task_id=f'airbyte_abs_{next_key}',
@@ -70,27 +74,39 @@ with DAG(
         ) for next_key, next_val in airbyte_abs_endpoints.items()
     ]
 
-    # # Task setup for Databricks - ABS. Dynamic creation of tasks based on datasets above.
-    # databricks_abs_tasks = [
-    # DatabricksRunNowOperator(
-    #         task_id=f'databricks_task_abs_{next_param["dataset"]}',
-    #         job_name=databricks_abs_job_name,
-    #         notebook_params = next_param,
-    #         timeout=300,
-    #         databricks_conn_id=databricks_airflow_conn
-    #     ) for next_param in databricks_abs_params]
+    # Task setup for Databricks - ABS. Just one per layer needed.
+    databricks_task_abs_bronze = DatabricksRunNowOperator(
+        task_id='databricks_abs_bronze',
+        job_name=databricks_abs_bronze_job_name,
+        databricks_conn_id=databricks_airflow_conn
+    )
 
-    # # Task setup for Databricks - Domain. Just one needed.
+    databricks_task_abs_silver = DatabricksRunNowOperator(
+        task_id='databricks_abs_silver',
+        job_name=databricks_abs_silver_job_name,
+        databricks_conn_id=databricks_airflow_conn
+    )
+
+    # Task setup for Databricks - Domain. Just one per layer needed.
     databricks_task_domain_bronze = DatabricksRunNowOperator(
         task_id='databricks_domain_bronze',
-        job_name = databricks_domain_bronze_job_name,
+        job_name=databricks_domain_bronze_job_name,
         databricks_conn_id=databricks_airflow_conn
     )
 
     databricks_task_domain_silver = DatabricksRunNowOperator(
         task_id='databricks_domain_silver',
-        job_name = databricks_domain_silver_job_name,
+        job_name=databricks_domain_silver_job_name,
         databricks_conn_id=databricks_airflow_conn
     )
 
-    airbyte_abs_tasks >> airbyte_task_domain >> databricks_task_domain_bronze >> databricks_task_domain_silver
+    # Final combined dataset
+    databricks_task_combined_gold = DatabricksRunNowOperator(
+        task_id='databricks_combined_gold',
+        job_name=databricks_combined_gold_job_name,
+        databricks_conn_id=databricks_airflow_conn
+    )
+
+    airbyte_abs_tasks >> databricks_task_abs_bronze >> databricks_task_abs_silver
+    airbyte_task_domain >> databricks_task_domain_bronze >> databricks_task_domain_silver
+    [databricks_task_abs_silver, databricks_task_domain_silver] >> databricks_task_combined_gold
